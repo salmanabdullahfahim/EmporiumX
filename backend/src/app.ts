47bcdc1e272b7be583b8f10c8e72990ff6b030d7
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-12-02 18:28:13 +0600

    🌱 added a catchAsync reusable function for handling request response

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,10 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-require('dotenv').config();
-// import router from './app/routes';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+require("dotenv").config();
+import router from "./app/routes";
 // import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import cookieParser from 'cookie-parser';
-console.log(process.env.DATABASE_URL)
+import cookieParser from "cookie-parser";
+console.log(process.env.DATABASE_URL);
 
 const app: Application = express();
 app.use(cors());
@@ -14,29 +14,25 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-
-
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        Message: "ShopEase server is running correctly"
-    })
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "ShopEase server is running correctly",
+  });
 });
 
-// app.use('/api/v1', router);
+app.use("/api/v1", router);
 
 // app.use(globalErrorHandler);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(404).json({
-        success: false,
-        message: "API NOT FOUND!",
-        error: {
-            path: req.originalUrl,
-            message: "Your requested path is not found!"
-        }
-    })
-})
+  res.status(404).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
+  });
+});
 
 export default app;
