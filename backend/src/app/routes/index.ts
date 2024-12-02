f17dfc8a8ab4c510170581a633d9f4799e5fca9e
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-12-02 18:44:56 +0600

    🥅 added a global error handler for catching any types of error

--- backend/src/app/routes/index.ts
+++ backend/src/app/routes/index.ts
@@ -2,13 +2,13 @@ import express from "express";
 
 const router = express.Router();
 
-const moduleRoutes = [
-  {
-    path: "/user",
-    route: userRoutes,
-  },
-];
+// const moduleRoutes = [
+//   {
+//     path: "/user",
+//     route: userRoutes,
+//   },
+// ];
 
-moduleRoutes.forEach((route) => router.use(route.path, route.route));
+// moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
