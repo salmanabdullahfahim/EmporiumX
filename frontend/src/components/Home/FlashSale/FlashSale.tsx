3b35b770fdd0b052dd45c8f1b161c95413e991fa
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-12-05 15:33:46 +0600

    Update FlashSale component to handle empty data state
    - Added conditional rendering to display a message when no flash sales data is available.
    - Improved user experience by informing users when there are no items to show in the Flash Sale section.

--- frontend/src/components/Home/FlashSale/FlashSale.tsx
+++ frontend/src/components/Home/FlashSale/FlashSale.tsx
@@ -9,11 +9,15 @@ const FlashSale = async () => {
   return (
     <div className="md:mx-12 my-12">
       <h2 className="italic text-4xl font-semibold mb-4">Flash Sale⚡</h2>
-      <div className=" mx-12 grid items-center justify-center space-y-4 px-2 py-10 md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-4">
-        {flashSales.data.map((flashSale: FlashSaleType) => (
-          <FlashSaleCard {...flashSale} key={flashSale.id} />
-        ))}
-      </div>
+      {flashSales.data.length <= 0 ? (
+        <p className="text-2xl text-center my-4">No data found</p>
+      ) : (
+        <div className=" mx-12 grid items-center justify-center space-y-4 px-2 py-10 md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-4">
+          {flashSales.data.map((flashSale: FlashSaleType) => (
+            <FlashSaleCard {...flashSale} key={flashSale.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
